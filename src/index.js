d9d54e874fc945964f38a8a5564ee78773ed9860
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2020-01-14 16:28:09 +0700

    Setup redux provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from 'views/layout/App';
+import configureStore from 'state/redux/configureStore';
+import rootSaga from 'state/redux/rootSaga';
+import 'index.css';
+import * as serviceWorker from 'serviceWorker';
+
+const store = configureStore();
+store.runSaga(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
